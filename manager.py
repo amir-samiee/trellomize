da2af9d79f1ee7d51e8006557b5597d0904a8b74
arshiasamimi <samimarsh44@gmail.com>
2024-05-12 22:29:55 +0330

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,5 +1,6 @@
 import argparse
 import tools
+import base
 
 
 class SystemManager:
@@ -74,6 +75,18 @@ class SystemManager:
             print("No admin exists")
             return False
 
+    def ban(self, username: str):
+        if self.is_admin():
+            # deactivating the user
+            print(f"User {username} banned")
+
+    def view(self, substring=''):
+        user_dict = base.User.instances
+        i = 1
+        for username in user_dict.keys():
+            if substring in username:
+                print(f"{i+1}- {username}")
+
     # Creates a parser for managing system:
     def parser(self) -> argparse:
         # Creating the main parser:
@@ -81,7 +94,7 @@ class SystemManager:
         subparsers = parser.add_subparsers(
             dest="subcommand", title="subcommands")
 
-        # Creating admin parser
+        # Create admin parser
         create_admin_parser = subparsers.add_parser(
             name="create-admin", help="Creates an admin"
         )
@@ -112,14 +125,34 @@ class SystemManager:
 
         # Change Admin info parser:
         change_info_parser = admin_subparsers.add_parser(
-            name="change", help="Changes admin"
-        )
+            name="change", help="Changes admin")
         change_info_parser.add_argument(
             "-u", "--username", required=True, help="Admin's new username"
         )
         change_info_parser.add_argument(
             "-p", "--password", required=True, help="Admin's new password"
         )
+
+        # User parser:
+        user_parser = subparsers.add_parser(
+            name='user', help='User related tasks')
+        user_subparsers = user_parser.add_subparsers(
+            dest='user_subcommand', title='subcommands')
+
+        # Ban user parser:
+        ban_parser = user_subparsers.add_parser(name='ban', help='Ban users')
+        ban_parser.add_argument(
+            "-u", "--username", required=True, help="User's username"
+        )
+
+        # View users:
+        view_parser = user_subparsers.add_parser(
+            name='view', help='View users')
+        view_group = view_parser.add_mutually_exclusive_group()
+        view_group.add_argument(
+            '-a', '--all', action='store_true', default=False, help='View all users')
+        view_group.add_argument(
+            '-s', '--search', help='View users with a certain substring')
 
         # Purging data parser:
         purge_data_parser = subparsers.add_parser(
@@ -147,3 +180,12 @@ if __name__ == "__main__":
             manager.remove_admin(args.username, args.password)
         elif args.admin_subcommand == "change":
             manager.change(args.username, args.password)
+
+    elif args.subcommand == "user":
+        if args.user_subcommand == "view":
+            if not args.all:
+                manager.view(args.search)
+            else:
+                manager.view('')
+        elif args.user_subcommand == "ban":
+            manager.ban(args.username)
